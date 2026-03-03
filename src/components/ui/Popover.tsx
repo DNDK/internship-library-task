c4ba709f087016eeed6a14074bb4f5461a7d7926
DNDK <dubovec.andrey.2005@gmail.com>
2026-03-03 14:04:35 +0300

    some mobile adaptations

---
+++ src/components/ui/Popover.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+interface PopoverProps {
+  open: boolean;
+  onOpenChange: (val: boolean) => void;
+}
+
+export function PopoverTrigger() {}
+
+export function PopoverContent() {}
+
+export function PopoverOverlay() {}
+
+export function Popover() {}
